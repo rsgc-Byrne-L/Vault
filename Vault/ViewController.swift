5b82d6b5c6bb6bf92550d671856b1b30d45c7200
Liam Byrne <liammbyrnee@gmail.com>
2017-02-03 13:53:36 -0500

    Initial Commit

---
+++ Vault/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vault
+//
+//  Created by Liam Byrne on 2017-02-03.
+//  Copyright © 2017 Liam Byrne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
