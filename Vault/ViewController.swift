90134485598fef343cd62d46097b29217e995b58
Liam Byrne <liammbyrnee@gmail.com>
2017-02-22 16:00:36 -0500

    Finished both encryption and decryption #2

--- Vault/ViewController.swift
+++ Vault/ViewController.swift
@@ -9,8 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // Declare array that contains the alphabet, will be used to check the message
     var original = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    // Declare array that contains the cipher, which is a shift of 3 from original, will be used to encrypt the message
     var cipher = ["X","Y","Z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W"]
+    // Declare variables to use for counting
     var count = 0
     var letterCount = 0
 
@@ -20,30 +23,59 @@ class ViewController: UIViewController {
     @IBOutlet weak var encryptView: UITextView!
     @IBOutlet weak var decryptView: UITextView!
     @IBAction func doEncrypt(_ sender: UIButton) {
+        // Take user's input
         var encryptMessage: String? = encryptField.text
+        // Change user's input to all uppercase
         encryptMessage = encryptMessage?.uppercased()
-        encryptView.text = encryptMessage
+        // Store user's message length
         let messageCount : Int = (encryptMessage?.characters.count)!
+        // Declare array that stores each character of message
         var arrayMessage = encryptMessage?.characters.map { String($0) }
+        // Reset counting variables
+        count = 0
+        letterCount = 0
+        // Loop over the string
+        while count < messageCount {
+            // Loop over the alphabet
+            while letterCount < 26 {
+                if arrayMessage?[count] == original[letterCount] {
+                    arrayMessage?[count] = cipher[letterCount]
+                    letterCount = 26
+                } else {
+                letterCount += 1
+            }
+            }
+            letterCount = 0
+            count += 1
+        }
+        // Turn array back into string
+        let arrayMessageString = arrayMessage?.joined(separator: "")
+        // Display user's message
+        encryptView.text = arrayMessageString
+        
+    }
+    @IBAction func doDecrypt(_ sender: UIButton) {
+        var decryptMessage: String? = decryptField.text
+        decryptMessage = decryptMessage?.uppercased()
+        let messageCount : Int = (decryptMessage?.characters.count)!
+        var arrayMessage = decryptMessage?.characters.map { String($0) }
         count = 0
         letterCount = 0
         while count < messageCount {
             while letterCount < 26 {
-                print(letterCount)
-                print(count)
-                if arrayMessage?[count] == original[letterCount] {
-                    arrayMessage?[count] = cipher[letterCount]
+                if arrayMessage?[count] == cipher[letterCount] {
+                    arrayMessage?[count] = original[letterCount]
+                    letterCount = 26
+                } else {
+                    letterCount += 1
                 }
-                letterCount += 1
             }
             letterCount = 0
             count += 1
         }
         let arrayMessageString = arrayMessage?.joined(separator: "")
-        encryptView.text = arrayMessageString
+        decryptView.text = arrayMessageString
         
-    }
-    @IBAction func doDecrypt(_ sender: UIButton) {
     }
     
     override func viewDidLoad() {
