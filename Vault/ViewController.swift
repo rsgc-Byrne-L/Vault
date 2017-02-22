33979b9439f10e52de07ce092db7eb6671b39294
Liam Byrne <liammbyrnee@gmail.com>
2017-02-21 19:35:43 -0500

    Added outlets and actions for all buttons and text fields/views Started to store given data into variables

--- Vault/ViewController.swift
+++ Vault/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var encryptField: UITextField!
+    @IBOutlet weak var decryptField: UITextField!
+    @IBOutlet weak var encryptView: UITextView!
+    @IBOutlet weak var decryptView: UITextView!
+    @IBAction func doEncrypt(_ sender: UIButton) {
+        var encryptMessage: String? = encryptField.text
+        encryptView.text = encryptMessage
+    }
+    @IBAction func doDecrypt(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +31,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
